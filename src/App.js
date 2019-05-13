f237b35ea2a33b5ecf6d1bb040262a5bd9a01004
Unknown <reynatan21@gmail.com>
2019-05-13 20:19:13 +1000

    Remove unimelb header from entire app - only landing page and proposal form

--- src/App.js
+++ src/App.js
@@ -11,22 +11,16 @@ class App extends React.Component {
     render() {
         return (
             <Router>
-                <div className="uomcontent">
-                    <div className="page-inner">
-                        <div role="main">
-                            <Switch>
-                                <Route exact path='/'
-                                       component={LandingPage}/>
-                                <Route exact path='/submit'
-                                       component={SubmitPage}/>
-                                <Route exact path='/home'
-                                       component={Home}/>
-                                <Route exact path='/login'
-                                       component={Login}/>
-                            </Switch>
-                        </div>
-                    </div>
-                </div>
+                <Switch>
+                    <Route exact path='/'
+                           component={LandingPage}/>
+                    <Route exact path='/submit'
+                           component={SubmitPage}/>
+                    <Route exact path='/home'
+                           component={Home}/>
+                    <Route exact path='/login'
+                           component={Login}/>
+                </Switch>
             </Router>
         );
     }
