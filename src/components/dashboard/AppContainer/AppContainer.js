bfe2525cc56304281d184d98512c66432e0317a6
“Chamira <chamira.b@live.com.au>
2019-08-11 12:07:35 +1000

    added logout button functionality, removed avatar pictures - fixed changing proposal status functionality

--- src/components/dashboard/AppContainer/AppContainer.js
+++ src/components/dashboard/AppContainer/AppContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {withStyles} from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
@@ -12,100 +12,145 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import AssignmentIcon from "@material-ui/icons/Assignment";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import GavelIcon from "@material-ui/icons/Gavel";
 import PersonIcon from "@material-ui/icons/Person";
 import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
 import appContainerStyles from "./appContainerStyles";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
+import Button from "@material-ui/core/Button";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const styles = theme => appContainerStyles(theme);
 
 class AppContainer extends React.Component {
-    render() {
-        const {classes} = this.props;
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
 
-        return (
-            <div className={classes.root}>
-                <CssBaseline/>
-                <AppBar position="fixed" className={classes.appBar}>
-                    <Toolbar>
-                        <Typography variant="h6" color="inherit" noWrap>
-                            Name of open page
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-                <Drawer
-                    className={classes.drawer}
-                    variant="permanent"
-                    classes={{
-                        paper: classes.drawerPaper
-                    }}
-                    anchor="left"
+  _handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  _handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  _handleLogOut = () => {
+    // <Link to="/dashboard/proposals" className={classes.link} />;
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar />
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper
+          }}
+          anchor="left"
+        >
+          <div style={styles.avatar}>
+            <Grid container spacing={8} justify="flex-start">
+              <Grid item xs={5}>
+                <Typography
+                  variant="h6"
+                  align="center"
+                  gutterBottom
+                  className={classes.userTitle}
                 >
-                    <div style={styles.avatar}>
-                        <Grid container spacing={8} justify="flex-start">
-                            <Grid item xs={6}>
-                                <Avatar
-                                    alt="Remy Sharp"
-                                    src="https://www.w3schools.com/w3images/avatar2.png"
-                                    className={classes.avatar}
-                                />
-                            </Grid>
-                            <Grid item xs={5}>
-                                <Typography
-                                    variant="h6"
-                                    gutterBottom
-                                    className={classes.userTitle}
-                                >
-                                    User
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </div>
-                    <div/>
-                    <Divider/>
-                    <List>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <AssignmentIcon/>
-                            </ListItemIcon>
-                            <Link to="/dashboard" className={classes.link}>
-                                <ListItemText primary="View Proposals"/>
-                            </Link>
-                        </ListItem>
+                  User
+                </Typography>
+              </Grid>
+            </Grid>
+          </div>
+          <div />
+          <Divider />
+          <List>
+            <Link to="/dashboard" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <AssignmentIcon />
+                </ListItemIcon>
 
-                        <ListItem button>
-                            <ListItemIcon>
-                                <GavelIcon/>
-                            </ListItemIcon>
-                            <Link to="/dashboard/projects"
-                                  className={classes.link}>
-                                <ListItemText primary="View Projects"/>
-                            </Link>
-                        </ListItem>
-                    </List>
-                    <Divider/>
-                    <List>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <PersonIcon/>
-                            </ListItemIcon>
-                            <ListItemText primary="Log Out"/>
-                        </ListItem>
-                    </List>
-                </Drawer>
-                <main className={classes.content}>
-                    <div className={classes.toolbar}/>
-                    {this.props.children}
-                </main>
-            </div>
-        );
-    }
+                <ListItemText primary="View Proposals" />
+              </ListItem>
+            </Link>
+            <Link to="/dashboard/projects" className={classes.link}>
+              <ListItem button>
+                <ListItemIcon>
+                  <GavelIcon />
+                </ListItemIcon>
+
+                <ListItemText primary="View Projects" />
+              </ListItem>
+            </Link>
+          </List>
+          <Divider />
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <PersonIcon />
+              </ListItemIcon>
+              <ListItemText primary="Log Out" onClick={this._handleClickOpen} />
+            </ListItem>
+          </List>
+        </Drawer>
+        <Dialog
+          open={this.state.open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={this._handleClose}
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle id="alert-dialog-slide-title">
+            {"Are you sure you want to log out?"}
+          </DialogTitle>
+          <DialogContent />
+          <DialogActions>
+            <Link to="/" className={classes.link}>
+              <Button onClick={this._handleLogOut} color="primary">
+                Yes
+              </Button>
+            </Link>
+
+            <Button color="primary" onClick={this._handleClose}>
+              No
+            </Button>
+          </DialogActions>
+        </Dialog>
+
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          {this.props.children}
+        </main>
+      </div>
+    );
+  }
 }
 
 AppContainer.propTypes = {
-    classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(AppContainer);
--- src/components/dashboard/Projects/IndividualProject/ProjectInfo/Description.js
+++ src/components/dashboard/Projects/IndividualProject/ProjectInfo/Description.js
@@ -1,48 +1,45 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
-import {withStyles} from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
 
 const styles = {
-    description: {
-        overflow: 'auto',
-        textAlign: 'justify',
-        paddingLeft: 10,
-        marginLeft: 20,
-        height: 170,
-        width: 670,
-    }
+  description: {
+    overflow: "auto",
+    textAlign: "justify",
+    paddingLeft: 10,
+    marginLeft: 20,
+    height: 170,
+    width: 370
+  }
 };
 
 class Description extends React.Component {
+  render() {
+    const { classes, description } = this.props;
 
-    render() {
-        const {classes, description} = this.props;
-
-        return (
-            <div>
-                <Grid container direction='column'>
-                    <Grid item xs={2}>
-                        <Typography align="left" variant="h6">
-                            Description:
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={3} style={{marginTop: 10,}} align="center">
-                        <Paper className={classes.description}>
-                            {description}
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <Grid container direction="column">
+          <Grid item xs={2}>
+            <Typography align="left" variant="h6">
+              Description:
+            </Typography>
+          </Grid>
+          <Grid item xs={3} style={{ marginTop: 10 }} align="center">
+            <Paper className={classes.description}>{description}</Paper>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 Description.propTypes = {
-    classes: PropTypes.object.isRequired,
-    description: PropTypes.string.isRequired,
+  classes: PropTypes.object.isRequired,
+  description: PropTypes.string.isRequired
 };
 
 export default withStyles(styles)(Description);
