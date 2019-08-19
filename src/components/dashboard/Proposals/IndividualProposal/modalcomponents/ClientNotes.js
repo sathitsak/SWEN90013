d5ca272c9ca6f7bcd347d1eb25fb82651a5a3de2
“Chamira <chamira.b@live.com.au>
2019-08-19 15:55:47 +1000

    Fixing client modal

--- src/components/dashboard/Proposals/IndividualProposal/modalcomponents/ClientNotes.js
+++ src/components/dashboard/Proposals/IndividualProposal/modalcomponents/ClientNotes.js
@@ -2,8 +2,12 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import SnackbarContent from "@material-ui/core/SnackbarContent";
 import { withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
+  root: {
+    width: 900
+  },
   snackbar: {
     margin: theme.spacing.unit
   }
@@ -13,18 +17,15 @@ class ClientNotes extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <div>
-        <Grid container spacing={24}>
-          <Grid item xs={12} />
-          <SnackbarContent
-            className={classes.snackbar}
-            message="This is note 1"
-          />
-          <SnackbarContent
-            className={classes.snackbar}
-            message="This is note 2"
-          />
-        </Grid>
+      <div className={classes.root}>
+        <SnackbarContent
+          className={classes.snackbar}
+          message="This is note 1 fkalfkdjgjalfkjdajdlsakjfkdjsflkjaskfjksjk"
+        />
+        <SnackbarContent
+          className={classes.snackbar}
+          message="This is note 2"
+        />
       </div>
     );
   }
